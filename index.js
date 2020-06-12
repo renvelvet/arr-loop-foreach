9925d4974927fb7c687eb7a0928455e21d1369fb
Resha Puspita <reshapuspita734@gmail.com>
2020-06-12 20:51:51 +0700

    debug done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // cek 2 buah array
-const arr1 = ['Math', 'English', 'Programming']
-const arr2 = ['Geography', 'Spanish', 'Programming']
+const arr1 = ["Math", "English", "Programming"];
+const arr2 = ["Geography", "Spanish", "Programming"];
 
-arr1.forEach( item => {
-    arr2.forEach(item2 => {
-        console.log(item2 === item)
-    })
-})
+arr1.forEach((item) => {
+  arr2.forEach((item2) => {
+    if (item2 === item) {
+      console.log(item2 === item);
+    }
+  });
+});
